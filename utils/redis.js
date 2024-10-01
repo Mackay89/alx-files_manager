0797d2492f96e416298f7e3a7a9929fe3b03ed8a
Mackay89 <manotomackay@gmail.com>
2024-10-01 09:30:25 -0700

    task

--- utils/redis.js
+++ utils/redis.js
@@ -1,24 +1,25 @@
 import redis from 'redis'
 
 class RedisClient {
+  
   constructor() {
     this.client = redis.createClient({
       retry_strategy: (options) => {
         if (options.error && options.error.code === 'ECONNREFUSED') {
-          // End reconnecting on a specific error and flush all commands with an individual error
+          // End reconnecting on a specific error
           return new Error('The server refused the connection')
         }
         if (options.total_retry_time > 1000 * 60 * 60) {
-          // End reconnecting after a specific timeout and flush all commands with an individual error
+          // End reconnecting after a specific timeout
           return new Error('Retry time exhausted')
         }
         if (options.attempt > 10) {
-          // End reconnecting with built in error
+          // End reconnecting after several attempts
           return undefined
         }
-        // Reconnect after a delay
+        // reconnect after a delay
         return Math.min(options.attempt * 100, 3000)
-      },
+      }
     })
 
     this.client.on('error', (error) => {
