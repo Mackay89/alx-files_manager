d441b0c96c1ef6a1c0aa287368a3beff7af6b270
Mackay89 <manotomackay@gmail.com>
2024-10-01 09:20:47 -0700

    task

--- utils/redis.js
+++ utils/redis.js
@@ -1,4 +1,4 @@
-import redis from 'redis';
+import redis from 'redis'
 
 class RedisClient {
   constructor() {
@@ -6,64 +6,64 @@ class RedisClient {
       retry_strategy: (options) => {
         if (options.error && options.error.code === 'ECONNREFUSED') {
           // End reconnecting on a specific error and flush all commands with an individual error
-          return new Error('The server refused the connection');
+          return new Error('The server refused the connection')
         }
         if (options.total_retry_time > 1000 * 60 * 60) {
           // End reconnecting after a specific timeout and flush all commands with an individual error
-          return new Error('Retry time exhausted');
+          return new Error('Retry time exhausted')
         }
         if (options.attempt > 10) {
           // End reconnecting with built in error
-          return undefined;
+          return undefined
         }
-        // reconnect after
-        return Math.min(options.attempt * 100, 3000);
+        // Reconnect after a delay
+        return Math.min(options.attempt * 100, 3000)
       },
-    });
+    })
 
     this.client.on('error', (error) => {
-      console.error(`Redis client error: ${error}`);
-    });
+      console.error(`Redis client error: ${error}`)
+    })
   }
 
   isAlive() {
-    return this.client.connected;
+    return this.client.connected
   }
 
   async get(key) {
     return new Promise((resolve, reject) => {
       this.client.get(key, (error, reply) => {
         if (error) {
-          reject(error);
+          reject(error)
         } else {
-          resolve(reply);
+          resolve(reply)
         }
-      });
-    });
+      })
+    })
   }
 
   async set(key, value, durationInSeconds) {
     return new Promise((resolve, reject) => {
       this.client.setex(key, durationInSeconds, value, (error, reply) => {
         if (error) {
-          reject(error);
+          reject(error)
         } else {
-          resolve(reply);
+          resolve(reply)
         }
-      });
-    });
+      })
+    })
   }
 
   async del(key) {
     return new Promise((resolve, reject) => {
       this.client.del(key, (error, reply) => {
         if (error) {
-          reject(error);
+          reject(error)
         } else {
-          resolve(reply);
+          resolve(reply)
         }
-      });
-    });
+      })
+    })
   }
 
   // Gracefully quit the Redis connection
@@ -71,15 +71,15 @@ class RedisClient {
     return new Promise((resolve, reject) => {
       this.client.quit((error, reply) => {
         if (error) {
-          reject(error);
+          reject(error)
         } else {
-          resolve(reply);
+          resolve(reply)
         }
-      });
-    });
+      })
+    })
   }
 }
 
-const redisClient = new RedisClient();
-export default redisClient;
+const redisClient = new RedisClient()
+export default redisClient
 
